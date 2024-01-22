55b13d6bc93f4f371cacb96960929c02e78641a8
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-22 11:19:10 +0800

    pseudocode

--- create_new_list_from_two_list.py
+++ create_new_list_from_two_list.py
@@ -8,3 +8,11 @@
 
 # Expected Output:
 # result list: [25, 35, 40, 60, 90]
+
+# Pseudocode 
+
+# Create a function that will combine the odd from first list and even from second list
+# Create a variable that have empty list 
+# Create a loop and add condition to find the odd number in list1 
+# Create a loop and add condition to find the even number in list2
+# Input the given values in the lists and print the results 
